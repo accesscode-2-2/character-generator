a7f424e24e3624ee7eee28095c376dc87c1620b3
Derek <derek.netto@gmail.com>
2015-08-02 16:25:56 -0400

    added navigation bar

---
+++ CharacterGenerator/CharacterGenerator/SoldierListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SoldierListTableViewController.h
+//  CharacterGenerator
+//
+//  Created by Z on 8/2/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SoldierListTableViewController : UITableViewController
+
+@end
