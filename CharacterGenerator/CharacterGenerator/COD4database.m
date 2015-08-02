f97ab7b8eaed738406a184e8e7fe2564d562728d
Derek <derek.netto@gmail.com>
2015-08-02 15:36:53 -0400

    fixed perks

--- CharacterGenerator/CharacterGenerator/COD4database.m
+++ CharacterGenerator/CharacterGenerator/COD4database.m
@@ -82,7 +82,14 @@
 }
 
 -(void)setupPerks{
-    
+    NSDictionary *perks = @{
+                            @"TIER1" : @[@"C4 x2", @"RPG-7 x2", @"Special Grenades x3", @"Bomb Squad", @"Claymore x2", @"Bandolier", @"Frag x3"],
+                            
+                            @"TIER2" : @[@"Juggernaut", @"Sleight of Hand", @"Sonic Boom", @"Stopping Power", @"Double Tap", @"UAV Jammer", @"Overkill"],
+                            
+                            @"TIER3" : @[@"Deep Impact", @"Extreme Conditioning", @"Steady Aim", @"Last Stand", @"Martyrdom", @"Iron Lungs", @"Eavesdrop", @"Dead Silence"]
+                            };
+    self.perks = perks;
 }
 
 
