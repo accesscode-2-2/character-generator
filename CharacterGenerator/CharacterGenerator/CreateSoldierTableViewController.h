3b543310a91369a3641e4ce9e69dc32c1b81114d
Derek <derek.netto@gmail.com>
2015-08-02 17:55:24 -0400

    set up storyboard

---
+++ CharacterGenerator/CharacterGenerator/CreateSoldierTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateSoldierTableViewController.h
+//  CharacterGenerator
+//
+//  Created by Z on 8/2/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateSoldierTableViewController : UITableViewController
+
+@end
--- CharacterGenerator/CharacterGenerator/CreateSoldierTableViewController.m
+++ CharacterGenerator/CharacterGenerator/CreateSoldierTableViewController.m
@@ -1,39 +1,27 @@
 //
-//  SoldierListTableViewController.m
+//  CreateSoldierTableViewController.m
 //  CharacterGenerator
 //
 //  Created by Z on 8/2/15.
 //  Copyright (c) 2015 Mike Kavouras. All rights reserved.
 //
 
-#import "SoldierListTableViewController.h"
+#import "CreateSoldierTableViewController.h"
 
-@interface SoldierListTableViewController ()
+@interface CreateSoldierTableViewController ()
 
 @end
 
-@implementation SoldierListTableViewController
-
-- (IBAction)edit:(UIBarButtonItem *)sender {
-    [self setEditing:YES animated:YES];
-}
-
+@implementation CreateSoldierTableViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
     
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-//     self.navigationItem.leftBarButtonItem = self.editButtonItem;
-    
-//    UIBarButtonItem *addButton = [[UIBarButtonItem alloc ] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(add:)];
-//    
-//    self.navigationItem.rightBarButtonItem = addButton;
-    
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -46,22 +34,24 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 #warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 2;
+    return 0;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 3;
+    return 0;
 }
 
+/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SoldierIdentifier" forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
     
     // Configure the cell...
     
     return cell;
 }
+*/
 
 /*
 // Override to support conditional editing of the table view.
