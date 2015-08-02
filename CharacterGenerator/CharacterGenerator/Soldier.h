6304b441616804c93410afbfd20ce7587702d82f
Derek <derek.netto@gmail.com>
2015-08-02 15:49:50 -0400

    made soldier class

---
+++ CharacterGenerator/CharacterGenerator/Soldier.h
@@ -0,0 +1,22 @@
+//
+//  Soldier.h
+//  CharacterGenerator
+//
+//  Created by Z on 8/2/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Soldier : NSObject
+
+@property (nonatomic) NSString *primaryWeapon;
+@property (nonatomic) NSString *secondaryWeapon;
+@property (nonatomic) NSString *specialGrenade;
+@property (nonatomic) NSString *team;
+
+@property (nonatomic) NSArray *perks;
+
+
+
+@end
