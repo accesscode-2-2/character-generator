c994ac79bd890d26e971cccdef8ada60544bf2a0
Justine Gartner <GartnerJustine@gmail.com>
2015-08-02 15:55:02 -0400

    added custom classes for 3 main views

---
+++ CharacterGenerator/CharacterGenerator/DestinationDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DestinationDetailViewController.h
+//  CharacterGenerator
+//
+//  Created by Justine Gartner on 8/2/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DestinationDetailViewController : UIViewController
+
+@end
---
+++ CharacterGenerator/CharacterGenerator/VacayListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  VacayListTableViewController.h
+//  CharacterGenerator
+//
+//  Created by Justine Gartner on 8/2/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VacayListTableViewController : UITableViewController
+
+@end
