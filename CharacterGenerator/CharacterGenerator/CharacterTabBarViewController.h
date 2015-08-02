cd132f84aaa407f6567446a7bb356afff2ab794f
Artur Lane <arturlan.us@gmail.com>
2015-08-02 16:36:10 -0400

    Added TableViewController and TabBarController

---
+++ CharacterGenerator/CharacterGenerator/CharacterTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTabBarViewController.h
+//  CharacterGenerator
+//
+//  Created by Artur Lan on 8/2/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CharacterTabBarViewController : UITabBarController
+
+@end
