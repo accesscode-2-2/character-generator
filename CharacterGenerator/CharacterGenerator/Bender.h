ee7c12ba24ca3e00003492b6c9551ec53e1d72cf
Christian <maldonadoguitar@gmail.com>
2015-08-04 17:42:26 -0400

    images

---
+++ CharacterGenerator/CharacterGenerator/Bender.h
@@ -0,0 +1,13 @@
+//
+//  Bender.h
+//  CharacterGenerator
+//
+//  Created by Christian Maldonado on 8/4/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Bender : NSObject
+
+@end
---
+++ CharacterGenerator/CharacterGenerator/Bender.m
@@ -0,0 +1,13 @@
+//
+//  Bender.m
+//  CharacterGenerator
+//
+//  Created by Christian Maldonado on 8/4/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import "Bender.h"
+
+@implementation Bender
+
+@end
