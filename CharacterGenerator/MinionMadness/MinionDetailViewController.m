00a53ac6dcaa41cf59acaf21e8a08e535667feb7
Zoufishan Mehdi <zzmehdi@gmail.com>
2015-08-08 12:57:19 -0400

    audio added

--- CharacterGenerator/MinionMadness/MinionDetailViewController.m
+++ CharacterGenerator/MinionMadness/MinionDetailViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MinionDetailViewController.h"
+#import <AVFoundation/AVFoundation.h> //mp3
 
 @interface MinionDetailViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
@@ -16,14 +17,27 @@
 @property (weak, nonatomic) IBOutlet UILabel *bodyTypeLabel;
 @property (weak, nonatomic) IBOutlet UILabel *heightLabel;
 
-
+@property (nonatomic) AVAudioPlayer *minionSong;
 
 @end
 
 @implementation MinionDetailViewController
 
+- (void)viewWillAppear:(BOOL)animated {
+    
+    [self.minionSong play]; // insert this wherever you want minionsong to play!
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    NSString *path = [NSString stringWithFormat:@"%@/minionsong2.mp3", [[NSBundle mainBundle] resourcePath]];
+    NSURL *soundUrl = [NSURL fileURLWithPath:path];
+    
+    
+    self.minionSong = [[AVAudioPlayer alloc] initWithContentsOfURL:soundUrl error:nil];
+    
     self.nameLabel.text = self.minionName;
     self.imageView.image = [UIImage imageNamed: self.minionName];
     
@@ -33,6 +47,9 @@
     self.heightLabel.text = [NSString stringWithFormat:@"%d", self.height];
     
     self.navigationItem.title = @"Minion";
+   
 }
+
+
 
 @end
